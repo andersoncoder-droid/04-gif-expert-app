be4025361e85ba4cf446dc53d947b21bad3616fc
Anderson Losada Coder <andersonlosada.11@gmail.com>
2025-05-13 04:06:24 -0500

    Add initial implementation of GifExpertApp with categories and styles

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+
 export const GifExpertApp = () => {
+  const [categories, setcategories] = useState(['One Punch', 'Dragon Ball']);
+
+  console.log(categories);
+
   return (
     <>
+      {/* titulo */}
       <h1>GiftExpertApp</h1>
+      {/* input */}
+      {/* listado de gif */}
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+      {/* gif item */}
     </>
   );
 };
